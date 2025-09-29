38c36c5703994103c98d94a84601d46983fe08e8
Santiago Orduz <s.orduz@uniandes.edu.co>
2025-09-28 22:54:40 -0500

    bono

--- bookstore-front/next.config.js
+++ bookstore-front/next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+const BACKEND_URL = process.env.BACKEND_URL || "http://127.0.0.1:8080";
+
 const nextConfig = {
+  output: "standalone",               
   async rewrites() {
-    return [
-      { source: '/api/:path*', destination: 'http://127.0.0.1:8080/api/:path*' },
-    ];
+    return [{ source: "/api/:path*", destination: `${BACKEND_URL}/api/:path*` }];
+  },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" },
+    ],
   },
 };
 
